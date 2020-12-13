0fad9a5a4e32bf01354746b03f8d538cf5eb3325
cl984430825 <984430825@qq.com>
2020-12-13 11:32:51 +0800

    login++

---
+++ pages/login/login.js
@@ -0,0 +1,75 @@
+//获取应用实例
+const app = getApp()
+const request = require('../../utils/request.js');
+
+Page({
+  data: {
+    loginForm: {},
+    loginRules: [
+      {
+        name: 'username',
+        rules: {required: true, message: '用户名不能为空'},
+      },
+      {
+        name: 'password',
+        rules: {required: true, message: '密码不能为空'},
+      }
+    ]
+  },
+  onLoad: function () {
+    
+  },
+  // 去注册
+  toRegister: function (){
+    wx.navigateTo({
+      url: '/pages/register/register',
+    })
+  },
+  formInputChange(e) {
+    const {field} = e.currentTarget.dataset;
+    this.setData({
+      [`loginForm.${field}`]: e.detail.value
+    })
+  },
+  // 点击登录按钮
+  clickBtnLogin: function () {
+    this.selectComponent("#loginForm").validate((valid, errors) => {
+      if (valid) {
+        this.confirmLogin()
+      }else{
+        const firstError = Object.keys(errors)
+        if (firstError.length) {
+          this.setData({
+            error: errors[firstError[0]].message
+          })
+        }
+      }
+    })
+  },
+  // 登录
+  confirmLogin: function(){
+    const url = `${app.baseUrl}/login`;
+    const method = "POST";
+    let data = {
+      username: this.data.loginForm.username,
+      password: this.data.loginForm.password
+    }
+    request({
+      url,
+      method,
+      data,
+      success: (res) => {
+        console.log(res)
+        if(res.data.status === "000"){
+          const { user_name, user_create_time, token } = res.data.data;
+          wx.setStorageSync('token', token);
+          wx.setStorageSync('user_name', user_name);
+          wx.setStorageSync('user_create_time', user_create_time);
+          wx.navigateBack({
+            delta: 1,
+          })
+        }
+      }
+    })
+  }
+})
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,151 +1,30 @@
-//index.js
 //获取应用实例
-const app = getApp()
-const request = require('../../utils/request.js');
+// const app = getApp()
+// const request = require('../../utils/request.js');
 
 Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
-    loginDialog: false,
-    registerDialog: false,
-    buttons: [],
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    showTopTips: false,
-    loginForm: {},
-    registerForm: {},
-    loginRules: [
-      {
-        name: 'username',
-        rules: {required: true, message: '用户名不能为空'},
-      },
-      {
-        name: 'password',
-        rules: {required: true, message: '密码不能为空'},
-      }
-    ],
-    registerRules: [
-      {
-        name: 'username',
-        rules: {required: true, message: '用户名不能为空'},
-      },
-      {
-        name: 'password',
-        rules: {required: true, message: '密码不能为空'},
-      },
-      {
-        name: 'conFirmPwd',
-        rules: {required: true, message: '密码不能为空'},
-      }
-    ]
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
+    
+  },
+  onShow: function () {
+    const storageInfo = {
+      user_name: wx.getStorageSync("user_name")
+    }
+    if(wx.getStorageSync("user_name")){
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+        userInfo: storageInfo
       })
     }
   },
-  // 打开Login弹窗
+  // 去登录/注册
   toLogin: function () {
-    this.setData({
-      loginDialog: true,
-      buttons: [{text: '去注册'}, {text: '确定'}]
-    })
-  },
-  formInputChange(e) {
-    let fornName = this.data.loginDialog ? 'loginForm' : 'registerForm'
-    const {field} = e.currentTarget.dataset
-    this.setData({
-      [`${fornName}.${field}`]: e.detail.value
-    })
-  },
-  loginDialogButton: function (e) {
-    const btnIdx = e.detail.index;
-    console.log(e)
-    if(btnIdx === 1){
-      let fornName = this.data.loginDialog ? 'loginForm' : 'registerForm'
-      this.selectComponent(`#${fornName}`).validate((valid, errors) => {
-        if (valid && fornName === "loginForm") {
-          this.confirmLogin()
-        }else if(valid && fornName === "registerForm"){
-          this.confirmRegister()
-        }else{
-          const firstError = Object.keys(errors)
-          if (firstError.length) {
-            this.setData({
-              error: errors[firstError[0]].message
-            })
-          }
-        }
-      })
-    }else{
-      let linkStr = !this.data.loginDialog ? '去注册' : '去登录';
-      this.setData({
-        buttons: [{text: linkStr}, {text: '确定'}],
-        loginDialog: !this.data.loginDialog,
-        registerDialog: !this.data.registerDialog
-      })
-    }
-  },
-  // 登录
-  confirmLogin: function(){
-    let url = "/login";
-    console.log(url)
-    request({
-      url,
-      success(res) {
-        console.log(res)
-        // resolve();
-      }
-    })
-  },
-  // 注册
-  confirmRegister: function(){
-    let url = "/register";
-    console.log(url)
-    request({
-      url,
-      success(res) {
-        console.log(res)
-        // resolve();
-      }
+    wx.navigateTo({
+      url: '/pages/login/login',
     })
   }
-  // getUserInfo: function(e) {
-  //   if(e.detail.errMsg.indexOf("ok") != -1){
-  //     app.globalData.userInfo = e.detail.userInfo
-  //     this.setData({
-  //       userInfo: e.detail.userInfo,
-  //       hasUserInfo: true
-  //     })
-  //   }
-  // }
 })
